0f92f763049e4f135c07ec5e562376f07e8be9c4
Mahmoud0090 <mmh.00090@gmail.com>
2022-03-21 00:26:07 +0300

    spawning Enemies

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public GameObject Enemy;
+    public Transform spawnPoint;
+
+    public float maxSpawnPointX;
+    public float enemySpawningWaitingTime;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+    void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnEnemies()
+    {
+        while (true)
+        {
+            Spawn();
+
+            yield return new WaitForSeconds(enemySpawningWaitingTime);
+        }
+    }
+
+    public void Spawn()
+    {
+        float randSpawnX = Random.Range(-maxSpawnPointX, maxSpawnPointX);
+
+        Vector3 enemySpawnPoint = spawnPoint.position;
+        enemySpawnPoint.x = randSpawnX;
+
+        Instantiate(Enemy, enemySpawnPoint, Quaternion.identity);
+
+    }
+}
